6e76c5c9a657851054db8129e92108b94d75e97d
Jared Hammett <jthammet@calpoly.edu>
2025-10-28 12:04:49 -0700

    fixing image build issue

--- packages/proto/vite.config.ts
+++ packages/proto/vite.config.ts
@@ -6,6 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "public",
+  publicDir: "public-assets",
   resolve: {
     alias: {
       "@": resolve(__dirname, "../src"),
